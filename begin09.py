1a5fec720104db798f2933ee1b7a074e5f477d95
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-26 14:23:52 +0500

    08

--- begin09.py
+++ begin09.py
@@ -1,11 +1,12 @@
 # Assign 8 (int value) to 'a' variable;
-
+a = 8
 # Assign 3 (int value) to 'b' variable;
-
+b = 3
 # Assign 2 (int value) to 'c' variable;
-
+c = 2
 # Assign 4 (int value) to 'd' variable;
-
+d = 4
 # Assign the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin09' to variable 'k';
-
+k = a + b -c * d
 # # Print the variable 'k'.
+print(k)
