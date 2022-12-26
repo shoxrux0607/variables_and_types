f2d6b6c8a14fad6142b96f78a344aec21637fa9e
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-26 14:19:10 +0500

    06

--- begin06.py
+++ begin06.py
@@ -1,9 +1,10 @@
 # Assign 24 (int value) to 'a' variable;
-
+a = 24
 # Assign 3 (int value) to 'b' variable;
-
+b = 3
 # Assign 5 (int value) to 'c' variable;
-
+c = 5
 # Assign the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin06' to variable 'k';
-
+k = a - b + c
 # # Print the variable 'k'.
+print(k)
