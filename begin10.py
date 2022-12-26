8faff7195e2d8103f00a53ac6445a2ac74ced8fd
Shoxrux Chulliev <chullievshoxrux0607@gmail.com>
2022-12-26 14:25:15 +0500

    10

--- begin10.py
+++ begin10.py
@@ -1,11 +1,12 @@
 # Assign 5 (int value) to 'a' variable;
-
+a = 5
 # Assign 4 (int value) to 'b' variable;
-
+b = 4
 # Assign 2 (int value) to 'c' variable;
-
+c = 2
 # Assign 8 (int value) to 'd' variable;
-
+d = 8
 # Assign the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin10' to variable 'k';
-
+k = 2*(a*b-c*d)
 # # Print the variable 'k'.
+print(k)
